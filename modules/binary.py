e91eefe71abd1f539ad01ff42ec440a1f4a4b6dd
harshkr212 <guptagamer8@gmail.com>
2026-03-22 17:31:45 +0530

    feature : validations

--- modules/binary.py
+++ modules/binary.py
@@ -1,12 +1,3 @@
-# ---- Harsh 035: Exceptions ----------
-class InvalidBinaryInputError(ValueError):
-    def __init__(self, value: str):
-        super().__init__(f"'{value}' is not a valid binary string")
-
-class NegativeBinaryResultError(ValueError):
-    def __init__(self):
-        super().__init__("Binary subtraction result cannot be negative")
-
 # -- Pritam 038 -------------------------------------------------------
 
 from exceptions import InvalidBinaryInputError, NegativeBinaryResultError
---
+++ modules/exceptions.py
@@ -0,0 +1,8 @@
+# ---- Harsh 035: Exceptions ----------
+class InvalidBinaryInputError(ValueError):
+    def __init__(self, value: str):
+        super().__init__(f"'{value}' is not a valid binary string")
+
+class NegativeBinaryResultError(ValueError):
+    def __init__(self):
+        super().__init__("Binary subtraction result cannot be negative")
