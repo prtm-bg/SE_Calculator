b1e7a23d1b6f7b3e61a2bea848ed484ff1e5c679
dGhoshIIEST <devleena.cs@faculty.iiests.ac.in>
2026-03-30 15:02:23 +0530

    Update calculator.py with mode

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
 class Calculator:
+     # mode can be 1: Fraction, 2: Bin, 3: Oct, 4: Hex, 5: Set, 6: Matrix, default  = 0
+    mode = 0
     def add(self, a, b):
         return a + b
     def subtract(self, a, b):
