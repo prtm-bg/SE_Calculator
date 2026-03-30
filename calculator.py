3e0f6490abf5f643400a6ef848d95bf725c04589
dGhoshIIEST <devleena.cs@faculty.iiests.ac.in>
2026-03-30 15:08:49 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 class Calculator:
     # mode can be 1: Fraction, 2: Bin, 3: Oct, 4: Hex, 5: Set, 6: Matrix, default  = 0
     mode = 0
+    
     def add(self, a, b):
         return a + b
     def subtract(self, a, b):
@@ -11,3 +12,8 @@ class Calculator:
         if b == 0:
             raise ValueError("Division by zero")
         return a / b
+
+    def evaluate(mode = 0):
+        #check the mode and based on its values execute for different mode
+        print('evaluate method to extend for multiple derived classes')
+
